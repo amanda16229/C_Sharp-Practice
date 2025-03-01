8aafa6ebedd8b786d5e396af7279ce30dbd7ea4d
Amanda <amanda.olyer@gmail.com>
2025-03-01 01:12:25 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main()
         {
-            Console.WriteLine("hello world");  
+            Console.Write("Enter your age: ");
+            int age = Int32.Parse(Console.ReadLine());
+            LoopEx(age);
+        }
+        static void LoopEx(int number){
+            for(int i = 1; i < 10; i++){
+                number += 10;
+                if(i == 1) {
+                    Console.Write("In " + i + " decade. You will be: " + number + "\n");
+                }
+                else {
+                    Console.Write("In " + i + " decades. You will be: " + number + "\n");
+                }
+            }
         }
     }
+
 }
